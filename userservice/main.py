ee7da8857ead2a6b5f55f0408540231a95f6059f
siam khalid <saimkhalid6220@gmail.com>
2024-07-09 08:09:07 +0000

    from dev-containers

--- userservice/main.py
+++ userservice/main.py
@@ -1,46 +1,18 @@
 from contextlib import asynccontextmanager
-from typing import AsyncGenerator, Optional , Annotated
+from typing import Annotated
 from fastapi import FastAPI,Depends
-from sqlmodel import Field, SQLModel, Session, create_engine
-from userservice import setting
+from sqlmodel import Session
+from userservice.models import User
+from userservice.db import getSession
+from userservice.routers import auth, manageUser
+from userservice.routers.auth import get_current_user
 
-
-class User(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    username: str = Field(index=True)
-    password: str = Field(index=True)
-
-class User_session(User):
-    acess_token:str
-
-connectionString = str(setting.DATABASE_URL).replace(
-    "postgresql" , "postgresql+psycopg"
+app = FastAPI(
+    title='User Service',
+    version='1.0.0'
 )
 
-engine = create_engine(connectionString , connect_args={})
+app.include_router(auth.router)
+app.include_router(manageUser.router)
 
-def create_db_and_table():
-    SQLModel.metadata.create_all(engine)
 
-@asynccontextmanager
-async def lifespan(app:FastAPI)-> AsyncGenerator[None , None]:
-    print("hooray  creating tables")
-    create_db_and_table()
-    yield
-
-app = FastAPI()
-
-def getSession():
-    with Session(engine) as session:
-        yield session
-
-@app.post('/user/register' , response_model = User)
-def register_user(user:User  ,session : Annotated[Session , Depends(getSession)])->User:
-    session.add(user)
-    session.commit()
-    session.refresh(user)
-    return user
-
-# @app.post('/user/login' , response_model=User_session)
-# def login_user(user:User , session:Annotated[Session , Depends(getSession])->User_session:
-               
